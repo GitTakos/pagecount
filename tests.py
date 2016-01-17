82eb910b8d32c70be8d9a6e757b21bb1404a25df
GitTakos <taksplace@gmail.com>
2016-01-16 23:20:48 -0800

    test hostname

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class AppTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/')
         print response.data
-        assert 'visitors' in response.data
+        assert .format(os.environ['HOSTNAME']) in response.data
 
 if __name__ == '__main__':
     unittest.main()
