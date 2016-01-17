c6e3150911d65361c6735362d9ea8f11a4560a5d
GitTakos <taksplace@gmail.com>
2016-01-16 22:56:26 -0800

    tests.py assert test

--- tests.py
+++ tests.py
@@ -5,14 +5,10 @@ import unittest
 
 class AppTestCase(unittest.TestCase):
 
-   def containsAny(str, set):
-        """Check whether 'str' contains ANY of the chars in 'set'"""
-        return 1 in [c in str for c in set]
-
    def test_root_text(self):
         tester = app.test_client(self)
         response = tester.get('/')
-        assert containsAny('visitors',response)
+        assert 'visitors' in response.data
 
 if __name__ == '__main__':
     unittest.main()
